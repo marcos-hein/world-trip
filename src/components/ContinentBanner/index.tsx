ea51f0b333dbc8f988f31e29192fa9f7c9f7bc8c
Marcos V. H. Pereira <60493029+marcos-hein@users.noreply.github.com>
2021-12-06 20:31:59 -0300

    ContinentBanner responsive

--- src/components/ContinentBanner/index.tsx
+++ src/components/ContinentBanner/index.tsx
@@ -10,14 +10,19 @@ export function ContinentBanner({ name, url }: ContinentBannerProps): JSX.Elemen
     <Flex
       w="100%"
       direction="column-reverse"
-      h="500px"
-      px="36"
+      h={{ base: "135px", md: "500px" }}
+      px={{ base: "4", md: "24", lg: "36" }}
       pb="14"
       bgImage={`url(${url})`}
       bgSize="cover"
       bgPosition="center"
     >
-      <Heading size="2xl" fontWeight="medium" color="gray.100">
+      <Heading
+        size="2xl"
+        fontWeight="medium"
+        fontSize={{ base: "20px", md: "36px" }}
+        color="gray.100"
+      >
         {name}
       </Heading>
     </Flex>
